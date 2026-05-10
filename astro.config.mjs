6c25b6a9c66b1f2a260288aadcea98d4d2fca309
Joseph Chekanoff <joe@chekcreative.com>
2026-05-09 20:47:01 -0400

    feat: Support 404 and robots

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,5 +5,12 @@ import sitemap from '@astrojs/sitemap';
 // https://astro.build/config
 export default defineConfig({
   site: 'https://chek.dev',
-  integrations: [sitemap()],
+  integrations: [
+    sitemap({
+      filter: (page) => {
+        const pathname = new URL(page).pathname;
+        return !pathname.startsWith('/tag/') && !pathname.startsWith('/category/');
+      },
+    }),
+  ],
 });
